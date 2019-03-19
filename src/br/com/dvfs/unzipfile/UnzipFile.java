1b5845291b1d7e99615c25a6d9c5f1f992bb4a9c
Douglas Vitor <doug.vitor@gmail.com>
2019-03-18 23:52:21 -0300

    Zip e Unzip, nada a ver com novidades do java 8

---
+++ src/br/com/dvfs/unzipfile/UnzipFile.java
@@ -0,0 +1,54 @@
+package br.com.dvfs.unzipfile;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class UnzipFile {
+
+	public static void main(String[] args) throws IOException {
+		
+		String fileZip = "compressed.zip";
+		
+		byte[] fileZipByte = Files.readAllBytes(new File(fileZip).toPath());
+		
+		File destDir = new File("unzipTest");
+		
+		byte[] buffer = new byte[1024];
+		ZipInputStream zis = new ZipInputStream(new ByteArrayInputStream(fileZipByte));
+		ZipEntry zipEntry = zis.getNextEntry();
+		
+		while (zipEntry != null) {
+			File newFile = newFile(destDir, zipEntry);
+			FileOutputStream fos = new FileOutputStream(newFile);
+			int len;
+			while ((len = zis.read(buffer)) > 0) {
+				fos.write(buffer, 0, len);
+			}
+			
+			fos.close();
+			zipEntry = zis.getNextEntry();
+		}
+		
+		zis.closeEntry();
+		zis.close();
+	}
+	
+	 public static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+	        File destFile = new File(destinationDir, zipEntry.getName());
+	         
+	        String destDirPath = destinationDir.getCanonicalPath();
+	        String destFilePath = destFile.getCanonicalPath();
+	         
+	        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+	            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+	        }
+	         
+	        return destFile;
+	    }
+
+}
